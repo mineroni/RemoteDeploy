877d2c172095e17f48f3a7ff363f3f7ae1e8c323
Rádai Ronald <gtadj.roni@gmail.com>
2024-11-11 11:07:09 +0100

    RemoteDeploy project added

---
+++ RemoteDeploy/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+
+namespace RemoteDeploy;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string mainProjectName = "HAL";
+
+        string[] extensionsToDelete = { "",
+                                        ".deps.json",
+                                        ".runtimeconfig.json",
+                                        ".pdb",
+                                        ".dll",
+                                      };
+
+        // Getting project names in the local directory
+        // If folder name starts with a dot, it is not a project, skip it
+        string[] projects = Directory.GetDirectories(".");
+        for (int i = 0; i < projects.Length; i++)
+            projects[i] = projects[i].Split('\\').Last();
+        projects = projects.Where(x => !x.StartsWith(".")).ToArray();
+
+        // Creating remove command for old files
+        string removeCommand = "rm";
+        foreach (string project in projects)
+            foreach (string file in extensionsToDelete)
+                removeCommand += $" debug/{project}{file}";
+
+        // Removing previous files
+        var proc = Process.Start("cmd.exe", $"/c \"ssh root@192.168.3.6 {removeCommand}\"");
+        proc.WaitForExit();
+
+        // Publishing new files
+        foreach (string project in projects)
+            foreach (string file in extensionsToDelete)
+                Process.Start("cmd.exe", $"/c \"scp {project}\\bin\\Debug\\net8.0\\linux-arm64\\publish\\{project}{file} root@192.168.3.6:debug)\"");
+
+        // Adding execution permission to the startup file
+        proc = Process.Start("cmd.exe", $"/c \"ssh root@192.168.3.6 chmod +x debug/{mainProjectName}\"");
+    }
+}
